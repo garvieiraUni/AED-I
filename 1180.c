ca58a51f56d1453eb72488ace906d8284032c8a9
garvieiraUni <gabrielarcanjo260107@gmail.com>
2025-10-06 20:08:47 -0300

    Update 1180.c

--- 1180.c
+++ 1180.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main() {
+    int menor, pos, n, i;
+    scanf("%d", &n);
+    int x[n];
+    for(i=0; i<n; i++){
+        scanf("%d", &x[i]);
+    }
+    for(i=0; i<n; i++){
+        if(i==0){
+            menor = x[i];
+            pos = 0;
+        }
+        else{
+            if(x[i]<menor){
+                menor = x[i];
+                pos = i;
+            }
+        }
+    }
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao: %d\n", pos);
+    return 0;
+}
