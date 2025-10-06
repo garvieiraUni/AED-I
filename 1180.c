bbd870745c4c08ca4fc6c75666d46e9e5b75a116
garvieiraUni <gabrielarcanjo260107@gmail.com>
2025-10-06 20:10:49 -0300

    Update 1180.c

--- 1180.c
+++ 1180.c
@@ -2,11 +2,14 @@
 
 int main() {
     int menor, pos, n, i;
+    //pega o tamanho do vetor
     scanf("%d", &n);
     int x[n];
+    //pega os valores do vetor
     for(i=0; i<n; i++){
         scanf("%d", &x[i]);
     }
+    //percorre o vetor e armazena o menor valor que aparecer em <menor> e sua posição em <pos>.
     for(i=0; i<n; i++){
         if(i==0){
             menor = x[i];
